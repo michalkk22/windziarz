b87e0ee6ba67ac4af61b22a3248b78f367679180
Michal <m.kolodziej2211@gmail.com>
2026-03-26 19:24:44 +0100

    constructors fix

--- shared/Messages.hpp
+++ shared/Messages.hpp
@@ -20,6 +20,13 @@ class Messages
 {
 public:
     Messages(int flags);
+
+    Messages(const Messages &) = delete;
+    Messages &operator=(const Messages &) = delete;
+
+    Messages(Messages &&) = default;
+    Messages &operator=(Messages &&) = default;
+
     void send(const Message &message) const;
     Message receive() const;
     void unlink() const;
