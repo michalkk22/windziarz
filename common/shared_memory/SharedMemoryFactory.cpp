416d7fca939388a462e0b407adfeb5bb23d09eeb
Michal <m.kolodziej2211@gmail.com>
2026-03-30 11:29:42 +0200

    moved shm factory implementation to cpp

---
+++ common/shared_memory/SharedMemoryFactory.cpp
@@ -0,0 +1,31 @@
+#include "SharedMemoryFactory.hpp"
+
+#include "shmNames.hpp"
+
+SharedMemory<States> SharedMemoryFactory::createStates()
+{
+    return SharedMemory<States>(SHM_STATES, true);
+}
+
+SharedMemory<States> SharedMemoryFactory::joinStates()
+{
+    return SharedMemory<States>(SHM_STATES);
+}
+
+SharedMemory<ElevatorQueueData> SharedMemoryFactory::createElevatorQueue(std::string id)
+{
+    SharedMemory<ElevatorQueueData> shm(SHM_ELEVATOR_QUEUE_PREFIX, true);
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+
+    pthread_mutex_init(&shm.get()->mutex, &attr);
+
+    return shm;
+}
+
+SharedMemory<ElevatorQueueData> SharedMemoryFactory::joinElevatorQueue(std::string id)
+{
+    return SharedMemory<ElevatorQueueData>(SHM_ELEVATOR_QUEUE_PREFIX + id);
+}
--- common/shared_memory/SharedMemoryFactory.hpp
+++ common/shared_memory/SharedMemoryFactory.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "shmNames.hpp"
 #include "SharedMemory.hpp"
 #include "States.hpp"
 #include "ElevatorQueueData.hpp"
@@ -10,31 +9,9 @@ class SharedMemoryFactory
 public:
     SharedMemoryFactory() = delete;
 
-    static SharedMemory<States> createStates()
-    {
-        return SharedMemory<States>(SHM_STATES, true);
-    }
+    static SharedMemory<States> createStates();
+    static SharedMemory<States> joinStates();
 
-    static SharedMemory<States> joinStates()
-    {
-        return SharedMemory<States>(SHM_STATES);
-    }
-
-    static SharedMemory<ElevatorQueueData> createElevatorQueue(std::string id)
-    {
-        SharedMemory<ElevatorQueueData> shm(SHM_ELEVATOR_QUEUE_PREFIX, true);
-
-        pthread_mutexattr_t attr;
-        pthread_mutexattr_init(&attr);
-        pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
-
-        pthread_mutex_init(&shm.get()->mutex, &attr);
-
-        return shm;
-    }
-
-    static SharedMemory<ElevatorQueueData> joinElevatorQueue(std::string id)
-    {
-        return SharedMemory<ElevatorQueueData>(SHM_ELEVATOR_QUEUE_PREFIX + id);
-    }
+    static SharedMemory<ElevatorQueueData> createElevatorQueue(std::string id);
+    static SharedMemory<ElevatorQueueData> joinElevatorQueue(std::string id);
 };
